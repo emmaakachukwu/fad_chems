75701074e35edbabe2e5c8e4c6e69c4d02a01aea
emmaakachukwu <emmanuelakachukwu1@gmail.com>
2021-01-04 21:28:09 +0100

    small change

--- components/footer.php
+++ components/footer.php
@@ -1,140 +1,140 @@
 <footer>
-        <!-- Footer Start-->
-        <div class="footer-area footer-padding">
-            <div class="container">
-                <div class="row d-flex justify-content-between">
-                    <div class="col-xl-3 col-lg-3 col-md-5 col-sm-6">
-                        <div class="single-footer-caption mb-50">
-                            <div class="single-footer-caption mb-30">
-                                <!-- logo -->
-                                <div class="footer-logo">
-                                    <a href="index.php"><img src="assets/img/logo/fad_logo.png" alt=""></a>
-                                </div>
-                                <div class="footer-tittle">
-                                    <div class="footer-pera">
-                                        <!-- <p>Asorem ipsum adipolor sdit amet, consectetur adipisicing elitcf sed do
-                                            eiusmod tem.</p> -->
-                                    </div>
-                                </div>
+    <!-- Footer Start-->
+    <div class="footer-area footer-padding">
+        <div class="container">
+            <div class="row d-flex justify-content-between">
+                <div class="col-xl-3 col-lg-3 col-md-5 col-sm-6">
+                    <div class="single-footer-caption mb-50">
+                        <div class="single-footer-caption mb-30">
+                            <!-- logo -->
+                            <div class="footer-logo">
+                                <a href="index.php"><img src="assets/img/logo/fad_logo.png" alt=""></a>
                             </div>
-                        </div>
-                    </div>
-                    <div class="col-xl-2 col-lg-3 col-md-3 col-sm-5">
-                        <div class="single-footer-caption mb-50">
                             <div class="footer-tittle">
-                                <h4>Quick Links</h4>
-                                <ul>
-                                    <li><a href="./index.php">Home</a></li>
-                                    <li><a href="./products.php">Products</a></li>
-                                    <li><a href="./payment.php">Make Payment</a></li>
-                                </ul>
+                                <div class="footer-pera">
+                                    <!-- <p>Asorem ipsum adipolor sdit amet, consectetur adipisicing elitcf sed do
+                                        eiusmod tem.</p> -->
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-                <!-- Footer bottom -->
-                <div class="row align-items-center">
-                    <div class="col-xl-7 col-lg-8 col-md-7">
-                        <div class="footer-copy-right">
-                            <p>
-                                Copyright &copy;<script>
-                                    document.write(new Date().getFullYear());
-                                </script> All rights reserved | <?php echo ucfirst($app_name) ?>
-                            </p>
+                <div class="col-xl-2 col-lg-3 col-md-3 col-sm-5">
+                    <div class="single-footer-caption mb-50">
+                        <div class="footer-tittle">
+                            <h4>Quick Links</h4>
+                            <ul>
+                                <li><a href="./index.php">Home</a></li>
+                                <li><a href="./products.php">Products</a></li>
+                                <li><a href="./payment.php">Make Payments</a></li>
+                            </ul>
                         </div>
                     </div>
                 </div>
             </div>
+            <!-- Footer bottom -->
+            <div class="row align-items-center">
+                <div class="col-xl-7 col-lg-8 col-md-7">
+                    <div class="footer-copy-right">
+                        <p>
+                            Copyright &copy;<script>
+                                document.write(new Date().getFullYear());
+                            </script> All rights reserved | <?php echo ucfirst($app_name) ?>
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
-        <!-- Footer End-->
-    </footer>
+    </div>
+    <!-- Footer End-->
+</footer>
 
-    <!-- JS here -->
+<!-- JS here -->
 
-    <script src="./assets/js/vendor/modernizr-3.5.0.min.js"></script>
-    <!-- Jquery, Popper, Bootstrap -->
-    <script src="./assets/js/vendor/jquery-1.12.4.min.js"></script>
-    <script src="./assets/js/popper.min.js"></script>
-    <script src="./assets/js/bootstrap.min.js"></script>
-    <!-- Jquery Mobile Menu -->
-    <script src="./assets/js/jquery.slicknav.min.js"></script>
+<script src="./assets/js/vendor/modernizr-3.5.0.min.js"></script>
+<!-- Jquery, Popper, Bootstrap -->
+<script src="./assets/js/vendor/jquery-1.12.4.min.js"></script>
+<script src="./assets/js/popper.min.js"></script>
+<script src="./assets/js/bootstrap.min.js"></script>
+<!-- Jquery Mobile Menu -->
+<script src="./assets/js/jquery.slicknav.min.js"></script>
 
-    <!-- Jquery Slick , Owl-Carousel Plugins -->
-    <script src="./assets/js/owl.carousel.min.js"></script>
-    <script src="./assets/js/slick.min.js"></script>
+<!-- Jquery Slick , Owl-Carousel Plugins -->
+<script src="./assets/js/owl.carousel.min.js"></script>
+<script src="./assets/js/slick.min.js"></script>
 
-    <!-- One Page, Animated-HeadLin -->
-    <script src="./assets/js/wow.min.js"></script>
-    <script src="./assets/js/animated.headline.js"></script>
-    <script src="./assets/js/jquery.magnific-popup.js"></script>
+<!-- One Page, Animated-HeadLin -->
+<script src="./assets/js/wow.min.js"></script>
+<script src="./assets/js/animated.headline.js"></script>
+<script src="./assets/js/jquery.magnific-popup.js"></script>
 
-    <!-- Scrollup, nice-select, sticky -->
-    <!--<script src="./assets/js/jquery.scrollUp.min.js"></script>-->
-    <script src="./assets/js/jquery.nice-select.min.js"></script>
-    <script src="./assets/js/jquery.sticky.js"></script>
+<!-- Scrollup, nice-select, sticky -->
+<!--<script src="./assets/js/jquery.scrollUp.min.js"></script>-->
+<script src="./assets/js/jquery.nice-select.min.js"></script>
+<script src="./assets/js/jquery.sticky.js"></script>
 
-    <!-- contact js -->
-    <script src="./assets/js/contact.js"></script>
-    <script src="./assets/js/jquery.form.js"></script>
-    <script src="./assets/js/jquery.validate.min.js"></script>
-    <script src="./assets/js/mail-script.js"></script>
-    <script src="./assets/js/jquery.ajaxchimp.min.js"></script>
+<!-- contact js -->
+<script src="./assets/js/contact.js"></script>
+<script src="./assets/js/jquery.form.js"></script>
+<script src="./assets/js/jquery.validate.min.js"></script>
+<script src="./assets/js/mail-script.js"></script>
+<script src="./assets/js/jquery.ajaxchimp.min.js"></script>
 
-    <!-- Jquery Plugins, main Jquery -->
-    <script src="./assets/js/plugins.js"></script>
-    <script src="./assets/js/main.js"></script>
+<!-- Jquery Plugins, main Jquery -->
+<script src="./assets/js/plugins.js"></script>
+<script src="./assets/js/main.js"></script>
 
-    <script type="text/javascript">
-        // Start of Tawk.to Script
-        var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-        (function(){
-        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-        s1.async=true;
-        s1.src='https://embed.tawk.to/5ff0ef4bdf060f156a938d23/1er2idg6l';
-        s1.charset='UTF-8';
-        s1.setAttribute('crossorigin','*');
-        s0.parentNode.insertBefore(s1,s0);
-        })();
-        // End of Tawk.to Script
+<script type="text/javascript">
+    // Start of Tawk.to Script
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/5ff0ef4bdf060f156a938d23/1er2idg6l';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
+    // End of Tawk.to Script
 
-        // GetButton.io widget
-        (function () {
-            var options = {
-                whatsapp: "+16012274580", // WhatsApp number
-                telegram: "managerFadchemicals", // Telegram bot username
-                call_to_action: "Message us", // Call to action
-                button_color: "#FF6550", // Color of button
-                position: "left", // Position may be 'right' or 'left'
-                order: "whatsapp,telegram", // Order of buttons
-            };
-            var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
-            var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
-            s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
-            var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
-        })();
-        // End of GetButton.io widget
+    // GetButton.io widget
+    (function () {
+        var options = {
+            whatsapp: "+16012274580", // WhatsApp number
+            telegram: "managerFadchemicals", // Telegram bot username
+            call_to_action: "Message us", // Call to action
+            button_color: "#FF6550", // Color of button
+            position: "left", // Position may be 'right' or 'left'
+            order: "whatsapp,telegram", // Order of buttons
+        };
+        var proto = document.location.protocol, host = "getbutton.io", url = proto + "//static." + host;
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+        s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+        var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+    })();
+    // End of GetButton.io widget
 
-        // custom google translate script
-        function changeGoogleStyles() {
-            if ( $(document).width() < 1000 ) {
-                if(($goog = $('.goog-te-menu-frame').contents().find('body')).length) {
-                    var stylesHtml = '<style>'+
-                        '.goog-te-menu2 {'+
-                            'max-width:100% !important;'+
-                            'overflow:scroll !important;'+
-                            'box-sizing:border-box !important;'+
-                            'height:auto !important;'+
-                        '}'+
-                    '</style>';
-                    $goog.prepend(stylesHtml);
-                } else {
-                    setTimeout(changeGoogleStyles, 50);
-                }
+    // custom google translate script
+    function changeGoogleStyles() {
+        if ( $(document).width() < 1000 ) {
+            if(($goog = $('.goog-te-menu-frame').contents().find('body')).length) {
+                var stylesHtml = '<style>'+
+                    '.goog-te-menu2 {'+
+                        'max-width:100% !important;'+
+                        'overflow:scroll !important;'+
+                        'box-sizing:border-box !important;'+
+                        'height:auto !important;'+
+                    '}'+
+                '</style>';
+                $goog.prepend(stylesHtml);
+            } else {
+                setTimeout(changeGoogleStyles, 50);
             }
         }
-        changeGoogleStyles();
-        // end of custom google translate script
-    </script>
+    }
+    changeGoogleStyles();
+    // end of custom google translate script
+</script>
 <!--  -->
 </body>
 
